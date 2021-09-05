23f94b99789886fec1e713903bcaf3abb7cc68a4
Gabriel <gabesenacom@gmail.com>
2021-09-05 20:26:31 -0300

    Fix typo when call title value

--- src/components/ScoreDisplay.js
+++ src/components/ScoreDisplay.js
@@ -2,7 +2,7 @@ const ScoreDisplay = (props) => {
   let {title, value} = props
 
   return (
-      <div className='score-display '+title>
+      <div className={'score-display '+title}>
         <h5>{title}</h5>
         <p>{value}</p>
       </div>
