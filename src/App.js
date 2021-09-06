de18f1d83cffb177258fe76b75c55c3b55011e5f
Gabriel <gabesenacom@gmail.com>
2021-09-05 21:25:47 -0300

    Add starting content on App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { useState } from 'react'
+import { createCardList } from './objects/CardCharactersBuilder'
+import Header from './components/Header'
+import DisplayScore from './components/DisplayScore'
+import PlayAgain from './components/PlayAgain'
+import CardList from './components/CardList'
 //import './App.css';
 
-function App() {
-  return null
+const App = () => {
+  let [score, setScore] = useState(0)
+  let [bestScore, setBestScore] = useState(0)
+  let [cards, setCards] = useState(createCardList())
+  let [showPlayAgain, setShowPlayAgain] = useState(false)
+
+  function handleCardClick (id, event) {
+    // card click
+  }
+
+  function handlePlayAgainClick () {
+    //play again click
+  }
+
+  function getRandomCardImage () {
+    return cards.sort(() => 0.5 - Math.random())[0].src
+  }
+
+  return (
+    <div id='app'>
+      <Header />
+      <main>
+        <DisplayScore scoreValue={score} bestScoreValue={bestScore} />
+        {showPlayAgain && (
+          <PlayAgain
+            clickHandler={handlePlayAgainClick}
+            image={getRandomCardImage()}
+          />
+        )}
+        <CardList cards={cards} clickHandler={handleCardClick} />
+      </main>
+    </div>
+  )
 }
 
-export default App;
+export default App
