6a5fd5cc467456ccfa5a634719fba48ec2489f0a
Gabriel <gabesenacom@gmail.com>
2021-09-06 20:10:35 -0300

    Add MessageCard styles and add classNames

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -1,9 +1,11 @@
+import './../styles/MessageCard.css'
+
 const MessageCard = props => {
   let { image, text } = props
   return (
     <div className='message-card'>
-      {image && <img src={image} alt={text} />}
-      <p>{text}</p>
+      {image && <img src={image} alt={text} className='message-card-icon'/>}
+      <p className='message-card-text'>{text}</p>
     </div>
   )
 }
